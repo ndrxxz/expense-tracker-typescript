69bdd7fdd5a6da3cb65533879c6fb26cf256f34b
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-21 17:49:58 +0800

    refactor: add icon

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -3,7 +3,10 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>💰Kwentado</div>
+      <div className={styles.title}>
+        <img src="./money.png" alt="money-logo" className={styles.icon} />
+        Kwentado
+      </div>
     </div>
   );
 }
