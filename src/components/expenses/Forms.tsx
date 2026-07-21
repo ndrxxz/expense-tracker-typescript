ab142dcbd687c96eba61dc05deadb4b496275a53
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-21 17:59:08 +0800

    feat: create forms component

---
+++ src/components/expenses/Forms.tsx
@@ -0,0 +1,10 @@
+
+export default function Forms() {
+  return (
+    <div className="bg-white/5 border border-white/10 rounded-xl p-5 flex flex-col gap-3">
+        {/* budget form */}
+        <div className="h-px bg-zinc-500 my-4"></div>
+        {/* expense form */}
+    </div>
+  )
+}
