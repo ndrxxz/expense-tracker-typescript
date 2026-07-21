b39ae639adbc8bb54472610d4c18820590eb4e6a
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-21 17:59:58 +0800

    refactor: add expenses component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Hero, Navbar } from "@/components/layout";
+import { Expenses } from "@/components/expenses";
 
 function App() {
   return (
     <div className="min-h-screen bg-zinc-950 text-white flex flex-col gap-10">
       <Navbar />
       <Hero />
+      <Expenses />
     </div>
   )
 }
