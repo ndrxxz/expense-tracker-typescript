3c5a0cee95e51670e62a8485b066ef7b24be7b4d
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-21 17:58:54 +0800

    feat: create expenses component

---
+++ src/components/expenses/Expenses.tsx
@@ -0,0 +1,15 @@
+import { Forms, Summary } from "@/components/expenses";
+
+export default function Expenses() {
+  return (
+    <div className="flex justify-center px-4">
+      <div className="flex flex-col gap-3 w-4xl">
+        <Summary budget={0} spent={0} remaining={0} />
+
+        <div className="grid grid-cols-2 gap-3 items-start">
+          <Forms />
+        </div>
+      </div>
+    </div>
+  );
+}
