3e3c01819bfe109e8dbcd2eef1b5e2d48fed12be
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-22 20:49:45 +0800

    refactor: update barrel file

--- src/components/expenses/index.ts
+++ src/components/expenses/index.ts
@@ -1,3 +1,4 @@
+export { default as BudgetForm } from "./BudgetForm";
 export { default as Expenses } from "./Expenses";
 export { default as Forms } from "./Forms";
 export { default as Summary } from "./Summary";
