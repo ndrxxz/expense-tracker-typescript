553de610e5e0a42fd1e3ff7eb7b3eb1089277900
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-24 17:59:15 +0800

    refactor: add expense form to the barrel file

--- src/components/expenses/index.ts
+++ src/components/expenses/index.ts
@@ -1,4 +1,5 @@
 export { default as BudgetForm } from "./BudgetForm";
+export { default as ExpenseForm } from "./ExpenseForm";
 export { default as Expenses } from "./Expenses";
 export { default as Forms } from "./Forms";
 export { default as Summary } from "./Summary";
