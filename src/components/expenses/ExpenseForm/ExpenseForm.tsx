f1136331b773264061993dbe060c41d99b3e36a1
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-24 17:57:31 +0800

    refactor: expense form
    - add form fields
    - pass props
    - make an interface

--- src/components/expenses/ExpenseForm/ExpenseForm.tsx
+++ src/components/expenses/ExpenseForm/ExpenseForm.tsx
@@ -1,8 +1,14 @@
-import { FormField } from "@/components/ui";
+import { Button, FormField, Select } from "@/components/ui";
 import styles from "./ExpenseForm.module.css";
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent, type SubmitEvent } from "react";
+import { categoryOptions } from "@/data";
+import type { Expense } from "@/types";
 
-export default function ExpenseForm() {
+interface ExpenseFormProps {
+  onAddExpense: (expense: Expense) => void;
+}
+
+export default function ExpenseForm({ onAddExpense }: ExpenseFormProps) {
   const [form, setForm] = useState({
     descInput: "",
     amountInput: "",
@@ -10,15 +16,42 @@ export default function ExpenseForm() {
     dateInput: "",
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { descInput, amountInput, categoryInput, dateInput } = form;
+
+    if (!descInput || !amountInput || !categoryInput || !dateInput) return;
+
+    const newExpense = {
+      id: Date.now(),
+      description: descInput,
+      amount: parseFloat(amountInput),
+      category: categoryInput,
+      date: dateInput,
+    }
+
+    onAddExpense(newExpense);
+
     setForm({
-      ...form,
-      [e.target.name]: e.target.value
+      descInput: "",
+      amountInput: "",
+      categoryInput: "",
+      dateInput: "",
     });
   }
 
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <div className={styles.title}>Add expenses</div>
 
       <FormField
@@ -29,9 +62,43 @@ export default function ExpenseForm() {
           type: "text",
           value: form.descInput,
           onChange: handleChange,
-          placeholder: "e.g. Jollibee lunch"
+          placeholder: "e.g. Jollibee lunch",
         }}
       />
+
+      <FormField
+        field={{
+          label: "Amount (₱)",
+          id: "input-amount",
+          name: "amountInput",
+          type: "number",
+          value: form.amountInput,
+          onChange: handleChange,
+          placeholder: "e.g. 0.00",
+        }}
+      />
+
+      <Select field={{
+        label: "Category",
+        id: "input-category",
+        name: "categoryInput",
+        options: categoryOptions,
+        value: form.categoryInput,
+        onChange: handleChange
+      }} />
+
+      <FormField
+        field={{
+          label: "Date",
+          id: "input-date",
+          name: "dateInput",
+          type: "date",
+          value: form.dateInput,
+          onChange: handleChange,
+        }}
+      />
+
+      <Button label={"+ Add expense"} />
     </form>
   );
 }
