16a37f5d96e910416c3f260c44cada73528fcf68
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-24 17:58:48 +0800

    refactor: add component
    - add expense form
    - make an interface

--- src/components/expenses/Forms.tsx
+++ src/components/expenses/Forms.tsx
@@ -1,11 +1,17 @@
-import BudgetForm from "./BudgetForm"
+import { BudgetForm, ExpenseForm } from "@/components/expenses"
+import type { Expense } from "@/types"
 
-export default function Forms() {
+interface FormsProps {
+  onSetBudget: (budget: number) => void;
+  onAddExpense: (expense: Expense) => void;
+}
+
+export default function Forms({ onSetBudget, onAddExpense }: FormsProps) {
   return (
     <div className="bg-white/5 border border-white/10 rounded-xl p-5 flex flex-col gap-3">
-        <BudgetForm />
+        <BudgetForm onSetBudget={onSetBudget} />
         <div className="h-px bg-zinc-500 my-4"></div>
-        {/* expense form */}
+        <ExpenseForm onAddExpense={onAddExpense} />
     </div>
   )
 }
