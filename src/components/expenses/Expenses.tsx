b84599c7e5951aef71ae5af82b48c016f80b6439
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-07-24 17:57:54 +0800

    refactor: add props

--- src/components/expenses/Expenses.tsx
+++ src/components/expenses/Expenses.tsx
@@ -7,7 +7,7 @@ export default function Expenses() {
         <Summary budget={0} spent={0} remaining={0} />
 
         <div className="grid grid-cols-2 gap-3 items-start">
-          <Forms />
+          <Forms onSetBudget={} onAddExpense={} />
         </div>
       </div>
     </div>
